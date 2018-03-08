53ee41a1cf7446eb90df18622e722b9b84743e14
TGIfr <mihail.lukjanec@gmail.com>
2018-03-08 20:22:43 +0200

    genre finding by mp3 url completed

---
+++ id3.py
@@ -0,0 +1,35 @@
+from urllib import request  # python 3
+from functools import reduce
+from io import BytesIO
+from mutagen.mp3 import MP3
+
+
+def get_genre(song_url):
+
+    def get_n_bytes(song_url, byte_len):
+        req = request.Request(song_url)
+        req.headers['Range'] = 'bytes=%s-%s' % (0, byte_len - 1)
+        response = request.urlopen(req)
+        return response.read()
+
+    data = get_n_bytes(song_url, 10)
+    if 'ID3' not in data[0:3].decode('ascii'):
+        print(data)
+        raise Exception('ID3 not in front of mp3 file')
+
+    size_encoded = bytearray(data[-4:])
+    size = reduce(lambda a, b: a*128+b, size_encoded, 0)
+
+    header = BytesIO()
+    # mutagen needs one full frame in order to function. Add max frame size
+    data = get_n_bytes(song_url, size + 2881)
+    header.write(data)
+    header.seek(0)
+    f = MP3(header)
+    # print(data)
+    # print(f.tags)
+
+    if f.tags and 'TCON' in f.tags.keys():
+        return f.tags['TCON'].text
+    else:
+        return 'Unknown'
--- main.py
+++ main.py
@@ -1,14 +1,37 @@
-import xmlHelper
-import nesting
+# import xmlHelper
+# import nesting
+#
+# links = xmlHelper.parse('links.xml')
+#
+# for l in links:
+#     print(l)
+#
+# xmlHelper.save('dich.xml', links)
+#
+# res = nesting.get_nested_links_from_list(links, 1)
+#
+# for l in res:
+#     print(l)
 
-links = xmlHelper.parse('links.xml')
 
-for l in links:
-    print(l)
+# def hrefs(url: str, nested: int = 1) -> set:
+#     if nested <= 0:
+#         return set()
+#     if nested == 1:
+#         soup = BeautifulSoup(urlopen(url))
+#         return set(a["href"] for a in soup.find_all("a"))
+#
+#     return reduce(lambda a, x: a or hrefs(url, nested - 1), hrefs(url), set())
+#
+#
+# list = hrefs(xmlHelper.parse('links.xml')[0])
+#
+# for i in list:
+#     print(list)
 
-xmlHelper.save('dich.xml', links)
 
-links = nesting.get_nested_links_from_list(links, 2)
+import id3
 
-for l in links:
-    print(l)
+url = 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3'
+x = id3.get_genre(url)
+print(x)
