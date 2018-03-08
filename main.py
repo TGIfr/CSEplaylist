0d2308eb58a927d38d6143eab6482a0c416486cf
mightyYaroslav <yaroslavsmth@gmail.com>
2018-03-09 01:33:33 +0200

    works

--- main.py
+++ main.py
@@ -11,15 +11,14 @@ links = xmlHelper.parse('links.xml')
 
 xmlHelper.save('dich.xml', links)
 # print(links)
-# links = nesting.get_nested_links_from_list(links, 2)
+# page_urls = nesting.get_nested_links_from_list(links, 2)
 # for l in links:
 #     print(l)
 # print(hrefs(links[0], nested=2))
-page_urls = reduce(lambda a, l: a or hrefs(l, nested=4), links, set())
+page_urls = reduce(lambda a, l: a or hrefs(l, nested=1), links, set())
 print('Page urls')
 print(page_urls)
 
-mp3urls = reduce(lambda a, url: a + mp3refs(url), list(page_urls)[0], [])
+mp3urls = reduce(lambda a, url: a + mp3refs(url), page_urls, [])
 print("mp3 urls")
 print(mp3urls)
-
