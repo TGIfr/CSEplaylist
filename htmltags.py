a968fcbfda40b376de3eac0d1981c4555d93086b
mightyYaroslav <yaroslavsmth@gmail.com>
2018-03-08 16:46:33 +0200

    fixed recursion and regex

--- htmltags.py
+++ htmltags.py
@@ -2,9 +2,14 @@ import re
 from functools import reduce
 from typing import List
 
-from bs4 import BeautifulSoup
+from musictools import musictools
+from bs4 import BeautifulSoup, UnicodeDammit
 from urllib import request
 from urllib import parse
+from stagger.id3 import *
+import mutagen
+import giofile
+from gi.repository import Gio
 
 
 def hrefs(url: str, nested: int = 1) -> set:
@@ -18,10 +23,16 @@ def hrefs(url: str, nested: int = 1) -> set:
     return reduce(lambda a, x: a or hrefs(url, nested - 1), hrefs(url), set())
 
 
-def genre(url: str) -> str:
-    return 'rock' + url
+def genre(url: str = '') -> str or None:
+    metadata = musictools.get_metadata('http://upload.wikimedia.org/wikipedia/commons/transcoded'
+                                       '/b/be/Anthem-of-Ukraine_Chorus_Veryovka.ogg/'
+                                       'Anthem-of-Ukraine_Chorus_Veryovka.ogg.mp3')
+    print('Metadata: ' + str(metadata))
+    return metadata['GENRE']
 
 
 def mp3refs(url: str) -> List[str]:
-    return re.findall(r"\b(https?)?(www)?.+?\.mp3\b", str(request.urlopen(url).read()))
-    #return re.findall(r"^(https?://)?(www)?[a-zA-Z0-9@:%_+.~#?/=]+\.mp3$", str(request.urlopen(url).read()))
+    soup = BeautifulSoup(request.urlopen(url), "html.parser")
+    unicode = UnicodeDammit(request.urlopen(url).read()).unicode_markup
+    soup.find_all("a", href=re.compile(r"\.mp3")) + soup.find_all("source", src=re.compile(r"\.mp3"))
+    return [el for el in soup.find_all(lambda tag: re.compile(r"\.mp3").match(tag.href) or re.compile(r"\.mp3").match(tag.src))]
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from htmltags import hrefs, mp3refs
+from htmltags import hrefs, mp3refs, genre
 
 #print(hrefs("http://www.apple.com", nested=1))
-print(mp3refs("https://ru.wikipedia.org/wiki/%D0%A3%D0%BA%D1%80%D0%B0%D0%B8%D0%BD%D0%B0"))
+# print(mp3refs("https://ru.wikipedia.org/wiki/%D0%A3%D0%BA%D1%80%D0%B0%D0%B8%D0%BD%D0%B0"))
+print(mp3refs("http://mp3party.net/music/8500219"))
+# print(genre())
