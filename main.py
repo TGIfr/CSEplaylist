e1abe80cad2baf63fd1d8feb155313a57efe206c
mightyYaroslav <yaroslavsmth@gmail.com>
2018-03-09 11:48:46 +0200

    added venv and progress bar

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from tqdm import tqdm, trange
 
 import xmlHelper
 import id3
@@ -7,11 +8,11 @@ from htmltags import hrefs, mp3refs
 links = xmlHelper.parse("links.xml")
 
 xmlHelper.save("dich.xml", links)
-page_urls = reduce(lambda a, l: a or hrefs(l, nested=2), links, set())
+page_urls = reduce(lambda a, l: a or hrefs(l, nested=2), tqdm(links, leave=False), set())
 print("Page urls")
 print(page_urls)
 
-mp3urls = reduce(lambda a, url: a + mp3refs(url), page_urls, [])
+mp3urls = reduce(lambda a, url: a + mp3refs(url), tqdm(page_urls, leave=False), [])
 print("mp3 urls")
 print(mp3urls)
-print(list(filter(lambda url: "Alternative" in id3.get_genre(url), mp3urls)))
+print(list(filter(lambda url: "Alternative" in id3.get_genre(url), tqdm(mp3urls, leave=False))))
